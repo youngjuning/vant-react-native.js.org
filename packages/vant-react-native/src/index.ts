6c1a78bb98ad345156dcbcdc6ba2440acb5d4afc
洛竹 <youngjuning@aliyun.com>
2021-07-07 19:14:20 +0800

    feat: add babel-plugin-import-vant

--- packages/vant-react-native/src/index.ts
+++ packages/vant-react-native/src/index.ts
@@ -1,4 +1,4 @@
 export { default as Icon } from '@vant-react-native/icons';
 export * from '@vant-react-native/icons';
 export { default as Button } from '@vant-react-native/button';
-export { default as babelPluginImportVant } from './utils/babel-plugin-import-vant';
+export { babelPluginImportVant } from 'babel-plugin-import-vant';
