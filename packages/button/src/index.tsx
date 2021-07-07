2f4197d57cbd51803475c7a0da0030f53f79a03c
洛竹 <youngjuning@aliyun.com>
2021-07-07 15:07:24 +0800

    chore: Init Button Component

---
+++ packages/button/src/index.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Button extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{ fontSize: 20, color: '#007fff' }}> WIP: Button Component </Text>
+      </View>
+    );
+  }
+}
---
+++ packages/vant-react-native/src/button/index.ts
@@ -0,0 +1,1 @@
+export { default as Button } from '@vant-react-native/button';
