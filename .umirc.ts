bd600ff5efbe58214d2edac13e4815f3fc080b99
洛竹 <youngjuning@aliyun.com>
2021-07-08 20:16:24 +0800

    docs: edit .umirc.ts

--- .umirc.ts
+++ .umirc.ts
@@ -13,6 +13,20 @@ export default defineConfig({
   },
   base: isProd ? '/vant-react-native': '/',
   publicPath: isProd ? 'https://cdn.jsdelivr.net/gh/youngjuning/vant-react-native@gh-pages/': '/',
+  exportStatic: {},
+  hash: true,
+  scripts: ['https://s9.cnzz.com/z_stat.php?id=1280093214&web_id=1280093214'],
+  styles: ['a[title=站长统计] { display: none; }'],
+  metas: [
+    {
+      name: 'keywords',
+      content: 'vant, react-native, vant-react-native',
+    },
+    {
+      name: 'description',
+      content: '轻量、可靠的 React Native 组件库',
+    },
+  ],
   navs: [
     null,
     {
