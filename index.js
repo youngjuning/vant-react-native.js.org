09a2c4d18e0869a62a5ccf5b0e4867dde7c60764
洛竹 <youngjuning@aliyun.com>
2021-07-09 13:33:58 +0800

    chore: rename App.tsx to index.tsx

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './example/App';
+import App from './example';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
